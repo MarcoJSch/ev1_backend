fe9c8312b396e35b612ad41d00171cd57b54837a
Marco Jara Schulz <mjaraschulz@gmail.com>
2025-10-20 16:53:00 -0300

    modificando admin
    se personalizaron las vistas para el administrador

--- registro_visitas/visitas/admin.py
+++ registro_visitas/visitas/admin.py
@@ -2,10 +2,27 @@ from django.contrib import admin
 from .models import Visita
 from .forms import VisitaForm
 
+@admin.register(Visita)
 class VisitaAdmin(admin.ModelAdmin):
     form = VisitaForm
     list_display = ('nombre', 'rut', 'estado', 'motivo', 'hora_entrada', 'hora_salida')
     search_fields = ('nombre', 'rut', 'motivo')
     list_filter = ('estado', 'hora_entrada')
+    list_editable = (
+        'estado',
+      # 'motivo',
+        'hora_salida',
+    )
 
-admin.site.register(Visita, VisitaAdmin)
+    fieldsets = (
+        ('Información Personal',{
+            'fields': ('nombre', 'rut')
+        }),
+        ('Información de la Visita',{
+            'fields': ('estado', 'motivo')
+        }),
+        ('Horarios',{
+            'fields': ('hora_entrada', 'hora_salida'),
+            'classes': ('collapse',)
+        })
+    )
